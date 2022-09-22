465a60536cc90a1122c209f05ee6d84a325fbfba
Suradit Luo <suradit_luo@cmu.ac.th>
2022-09-22 15:12:28 +0700

    3.6

---
+++ src/main/java/se331/rest/entity/EventDTO.java
@@ -0,0 +1,19 @@
+package se331.rest.entity;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventDTO {
+    Long id;
+    String category;
+    String title;
+    String description;
+    String location;
+    String date;
+    String time;
+    Boolean petAllowed;
+    EventOrganizerDTO organizer;
+}
